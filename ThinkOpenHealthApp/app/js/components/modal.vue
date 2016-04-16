94b2d0483736cb820245cc417d3802067ef0d582
itsacezon <aecay@upd.edu.ph>
2016-04-16 22:16:32 +0800

    It's over

---
+++ ThinkOpenHealthApp/app/js/components/modal.vue
@@ -0,0 +1,24 @@
+<template lang="jade">
+.modal-mask(v-show="show" transition="modal")
+  .modal-wrapper(@click="show = false")
+    .modal-container(@click.stop="true")
+      slot(name="content")
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      twoWay: true
+    }
+  },
+  ready () {
+    document.addEventListener("keydown", (e) => {
+      if (this.show && e.keyCode == 27)
+        this.show = false
+    })
+  }
+}
+</script>
